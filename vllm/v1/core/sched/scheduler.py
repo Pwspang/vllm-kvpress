d51423c652068ea25c1893b7d09cbb8113b0c75e
Colin Hong <colin.hong18@gmail.com>
2025-11-19 11:33:37 +0800

    Change logger for evictable token ranges map to debug

--- vllm/v1/core/sched/scheduler.py
+++ vllm/v1/core/sched/scheduler.py
@@ -551,7 +551,7 @@ class Scheduler(SchedulerInterface):
         for req in all_scheduled_reqs:
             if ranges := self.request_eviction_data.get(req.request_id.split("-")[-1]):
                 evictable_token_ranges_map[req.request_id] = ranges
-        logger.info(f"Evictable Token Ranges Map: {evictable_token_ranges_map}")
+        logger.debug(f"Evictable Token Ranges Map: {evictable_token_ranges_map}")
         grammar_bitmask = self.structured_output_manager.grammar_bitmask(
             self.requests,
             structured_output_request_ids,
