fc40a74e3c007f4a2130ab936951b059e2147f0a
Pang Wei Siong <weisiongp@gmail.com>
2025-11-30 12:21:53 +0800

    Bug fix for multiple sample generation

--- vllm/v1/core/sched/utils.py
+++ vllm/v1/core/sched/utils.py
@@ -10,7 +10,7 @@ from vllm.v1.request import Request, RequestStatus
 def check_stop(request: Request,
                max_model_len: int,
                pooler_output: Optional[torch.Tensor] = None) -> bool:
-    if (request.num_tokens >= max_model_len
+    if (request.num_tokens + request.num_dropped_tokens >= max_model_len
             or request.num_output_tokens >= request.max_tokens):
         request.status = RequestStatus.FINISHED_LENGTH_CAPPED
         return True
